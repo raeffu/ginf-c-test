91e3138c6871915948445a32bc3c98f802237551
Raphael Laubscher <raeffu@users.noreply.github.com>
2014-02-04 16:13:44 +0100

    Create primes.c

---
+++ primes.c
@@ -0,0 +1,17 @@
+/*
+  test.c
+  test exercises in C
+*/
+
+// include library
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  
+  int limit;
+
+  printf("List primes until: ");
+  scanf("%d", limit);
+
+  return 0;
+}
